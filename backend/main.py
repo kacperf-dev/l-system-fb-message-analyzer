54bccf667b1b7b2489c0644086ae9cf26c9e9a6d
kacperfilip <kacperf.dev@gmail.com>
2025-12-19 22:03:31 +0100

    feature: message data aggregation and preparation for l-system generation

--- backend/main.py
+++ backend/main.py
@@ -1,14 +1,24 @@
+import json
+import os
+
 from src.analysis.SentimentAnalyzer import SentimentAnalyzer
+from src.generators.TreeDataGenerator import TreeDataGenerator
 from src.utils import loaders
 
 
 def main():
-    conversation_data = loaders.load_message_data("data/")
-
     sentiment_analyzer = SentimentAnalyzer()
-    messages_with_sentiment = sentiment_analyzer.analyze(conversation_data)
+    tree_generator = TreeDataGenerator()
 
-    print(messages_with_sentiment)
+    conversation_data = loaders.load_message_data("data/")
+    messages_with_sentiment = sentiment_analyzer.analyze(conversation_data)
+    tree_data = tree_generator.generate_tree_data(messages_with_sentiment)
+
+    if not os.path.exists("out"):
+        os.makedirs("out")
+
+    with open("out/tree_data.json", "w") as f:
+        json.dump(tree_data, f, indent=4)
 
 
 if __name__ == "__main__":
